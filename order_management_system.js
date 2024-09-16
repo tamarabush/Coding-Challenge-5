4e84b372168a269de15bf7b157556d3c9e7aeaea
tamarabush <tamara.abush14@outlook.com>
2024-09-16 11:43:44 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,14 @@
+//TASK 1 - Create an Inventory Array of Product Objects:
+
+inventory = [
+    { name: 'americano', price: 5, quantity: 8 },
+    { name: 'latte', price: 7, quantity: 10 },
+    { name: 'affogato', price: 9, quantity: 7 },
+    { name: 'espresso', price: 8, quantity: 9}
+];
+//created an array of 4 coffee product objects
+
+
+console.log(inventory);
+//if you cannot see the inventory in the console.log, you can see it on the HTML page
+
