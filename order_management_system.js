63d26f7d50bc6c36229bf6dfaea5824c83400294
tamarabush <tamara.abush14@outlook.com>
2024-09-16 20:26:58 -0400

    Initialize orders array

--- order_management_system.js
+++ order_management_system.js
@@ -1,6 +1,6 @@
 //TASK 1 - Create an Inventory Array of Product Objects:
 
-inventory = [
+let inventory = [
     { name: 'americano', price: 5, quantity: 8 },
     { name: 'latte', price: 7, quantity: 10 },
     { name: 'affogato', price: 9, quantity: 7 },
@@ -12,3 +12,10 @@ inventory = [
 console.log(inventory);
 //if you cannot see the inventory in the console.log, you can see it on the HTML page
 
+
+
+//TASK 2 - Initialize orders array
+
+let orders = [];
+
+console.log(orders);
